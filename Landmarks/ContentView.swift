69d7c16a8e59b0c966725601f1856c1f2f9167f0
Stefan Peng <danrn79@me.com>
2020-04-04 18:20:54 -0500

    Initial commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Stefan Peng on 4/4/20.
+//  Copyright © 2020 Stefan Peng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
